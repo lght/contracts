069ea45802305fe77e7fe2871afb4159293f50ce
Cole Lightfighter <cole@parity.io>
2017-11-20 23:29:16 -0700

    Test for isLatest() getter
    Shows how to build abi-encoded function call to:
    - operations.isLatest(bytes32,bytes32)
    
    Basic unit test for whether isLatest() returns true for
    the encoded client and release.

--- test/manticore/contracts/TestOperations.sol
+++ test/manticore/contracts/TestOperations.sol
@@ -17,9 +17,27 @@ contract TestOperations
 		target_contract = _target;
 	}
 
+	function delegate(bytes _data)
+		public
+		payable
+	{
+		assert(target_contract.call.value(msg.value)(_data));
+	}
+
+	// Assert operations contract
+	//   is initialized
+	//   has a payable fallback function
 	function test_init()
 		public
+		payable
 	{
 		assert(target_contract.call.value(msg.value)());
+	}
+
+	// Assert operations.isLatest() returns true for a given client and release
+	function test_is_latest_returns_true(bytes is_latest)
+		public
+	{
+		assert(target_contract.call(is_latest));
 	}
 }
--- test/manticore/operations-concrete-unit-tests.py
+++ test/manticore/operations-concrete-unit-tests.py
@@ -40,3 +40,20 @@ test_contract_account.test_init()
 
 print(" contract_account %X"% (contract_account))
 print(" test_contract_account %X"% (test_contract_account))
+
+
+client = "parity"
+release = "1.8.2"
+print(" Testing target contract has latest release for")
+print("\t client: %s\n\t release: %s"% (client, release))
+     
+
+## Encode call to isLatest() to pass to our test contract
+is_latest_call = seth.make_function_call("isLatest(bytes32,bytes32)", client, release)
+## Make call to the test contract,
+##   delegate calls target_contract.isLatest(client, release)
+is_latest_ret = test_contract_account.test_is_latest_returns_true(is_latest_call)
+
+is_latest_error = "isLatest should return true for client: %s, release: %s"% (client, release)
+
+assert(is_latest_ret == True, is_latest_error)
